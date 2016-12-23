406c21aa696deb2b6c61e0a1fcbc60ad5d37b88e
井上正裕 <inouemasahiro@inouemasahiro-no-macbook-air.local>
2016-12-23 10:13:32 +0900

    Initial Commit

---
+++ calculation2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculation2
+//
+//  Created by 井上正裕 on 2016/12/23.
+//  Copyright © 2016年 井上正裕. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
